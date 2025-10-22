e38bcbea4239bdb748a6c7954779c7304a3f1637
phajy <andy.young@bristol.ac.uk>
2025-10-22 22:46:43 +0100

    feat: updated presentation plot and included a movie example

--- src/PresentationPlots.jl
+++ src/PresentationPlots.jl
@@ -2,12 +2,10 @@ using Gradus, Plots
 
 ### Thin disc geometry images ###
 ## Single spin and inclination angle 
-m = KerrMetric(1.0, 0.998)   #spacetime metric
+m = KerrMetric(1.0, 0.0)   #spacetime metric
 x = SVector(0.0, 1000.0, deg2rad(70), 0.0)    #position
-d = ThinDisc(0.0,400.0)    #disc geometry
+d = ThinDisc(Gradus.isco(m), 20.0)    #disc geometry thin disc from ISCO to 20 r_g
 λ_max = 2*x[2]
-α = range(-10.0, 10.0, 100)
-β = range(-10.0, 10.0, 100)
 
 #defining point function
 time_coord = PointFunction((m,gp,λ)-> gp.x[1]) #point function identifying time coordinate at the end of each geodesic
@@ -30,3 +28,25 @@ pf = time_coord ∘ filter_disc_intersect #creating reverse order list of the ge
     verbose = true)
 
 heatmap(α, β, image, aspect_ratio = 1)
+
+# animation of disc image over inclination angles
+# redshift point function
+redshift_pf = ConstPointFunctions.redshift(m, x) ∘ ConstPointFunctions.filter_intersected()
+anim = @animate for inc in vcat(2:2:88, 86:-2:4)
+    x = SVector(0.0, 1000.0, deg2rad(inc), 0.0)
+    α, β, image = rendergeodesics(
+        m, 
+        x, 
+        d, 
+        λ_max,
+        pf = redshift_pf, #point function (defining vel)
+        # image size
+        image_width = 800,
+        image_height = 400,
+        # impact parameter axes - 'zoom'
+        αlims = (-25, 25),
+        βlims = (-25, 25),
+        verbose = false)
+    heatmap(α, β, image, aspect_ratio = 1, title = "Inclination = $(inc)°", clims = (0, 1.3))
+end
+gif(anim, "thin_disc_inclination.gif", fps = 15)
